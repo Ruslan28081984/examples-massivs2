b52b348668f307420c2b2b4cb0623cb83e157d78
user1 <89201828020@mail.ru>
2022-07-27 19:04:32 +0300

    Выполненны задачи: 1, 2, 3, 4.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+      //  task123();
+        task4();
     }
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
@@ -14,20 +15,36 @@ public class Main {
         }
         return arr;
     }
-    public static void task1() {
+    public static void task123() {
         int[] arr = generateRandomArray();
+        Arrays.sort(arr);
+        int minNum = arr[0];
+        int maxNum = arr[arr.length - 1];
         int sumMonth = 0;
-        int minNum = 0;
-        int maxNum = 0;
-       // int[] arr = new int[30];
+        double averageValue = arr.length;
         for (int i = 0; i < arr.length; i++) {
             sumMonth += arr[i];
-            //System.out.println(arr[i]);
+            System.out.println(arr[i]);
         }
         System.out.print("сумма чисел  массива из " + arr.length + "-ти элементов равна: ");
         System.out.println(sumMonth);
+        averageValue = sumMonth / averageValue;
+        System.out.println(" максимальная трата в день: " + maxNum);
+        System.out.println(" минимальная трата в день: " + minNum);
+        System.out.println("Средняя сумма трат за месяц составила: " + averageValue + " рублей");
+    }
+    public static void task4(){
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length -1; i >= 0 ; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
+
+
+
+
+
 
 
 
