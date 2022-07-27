499857b6615b2a1c1861b3ab312e0c1ea1fb9474
Ruslan28081984 <89201828020@mail.ru>
2022-07-27 10:50:39 +0300

    первая задача

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,44 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-//        generateRandomArray();
-        example2();
-    }
-    int[] arr = generateRandomArray();
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random(100000);
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) ;
-        }
-        int sumMes = 0;
-        for (int element: arr) {
-            sumMes += element;
-            System.out.println(sumMes);
-        }
-        System.out.println(sumMes);
-       return arr;
-    }
-
-    public static void example2(){
-
-        int [] sum = new int [30];
+        generateRandomArray();
+        int[] arr = generateRandomArray();
         int sumMonth = 0;
-        Random random = new Random();
-        for (int i = 0; i < sum.length; i++) {
-            sum[i] = random.nextInt(100_000 ) +100_000;
-            sumMonth += sum[i];
-            System.out.println(sum[i]);
-        }
-        System.out.print("сумма чисел  массива из " + sum.length + "-ти элементов равна: ");
+
+        System.out.print("сумма чисел  массива из " + arr.length + "-ти элементов равна: ");
         System.out.println(sumMonth);
 
+
+
+    }
+        public static int[] generateRandomArray() {
+
+            Random random = new Random();
+            int sumMonth = 0;
+            int minNum = 0;
+            int maxNum = 0;
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+                sumMonth += arr[i];
+                //System.out.println(arr[i]);
+            }
+            return arr;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] < maxNum) {
+                    maxNum = arr[i];
+                    System.out.println(" максимальное значение: " + arr[i]);
+                }
+
+            }
+
+
+        }
+
     }
 
-}
+
+
 
 
 
