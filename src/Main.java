c1f02f9d056105f0f447764074211d97880eb2fe
Ruslan28081984 <89201828020@mail.ru>
2022-07-27 14:22:03 +0300

    сделал 1-ое задание,,,,

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,33 @@
 import java.util.Arrays;
 import java.util.Random;
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        generateRandomArray();
+        task1();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
         int[] arr = generateRandomArray();
         int sumMonth = 0;
-
+        int minNum = 0;
+        int maxNum = 0;
+       // int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            sumMonth += arr[i];
+            //System.out.println(arr[i]);
+        }
         System.out.print("сумма чисел  массива из " + arr.length + "-ти элементов равна: ");
         System.out.println(sumMonth);
-
-
-
     }
-        public static int[] generateRandomArray() {
-
-            Random random = new Random();
-            int sumMonth = 0;
-            int minNum = 0;
-            int maxNum = 0;
-            int[] arr = new int[30];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = random.nextInt(100_000) + 100_000;
-                sumMonth += arr[i];
-                //System.out.println(arr[i]);
-            }
-            return arr;
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] < maxNum) {
-                    maxNum = arr[i];
-                    System.out.println(" максимальное значение: " + arr[i]);
-                }
-
-            }
-
-
-        }
-
-    }
+}
 
 
 
